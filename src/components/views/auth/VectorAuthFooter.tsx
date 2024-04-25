2ad4d387f93b6dd13f364c3d84936c20e5d3bcbf
Codenia <devops@corewebconnections.com>
2024-04-25 15:47:07 +0530

    Fix GET-45
    Fix GET-45: Delete Links

--- src/components/views/auth/VectorAuthFooter.tsx
+++ src/components/views/auth/VectorAuthFooter.tsx
@@ -16,30 +16,30 @@ limitations under the License.
 */
 
 import React, { ReactElement } from "react";
-import SdkConfig from "matrix-react-sdk/src/SdkConfig";
+// import SdkConfig from "matrix-react-sdk/src/SdkConfig";
 
 import { _t } from "../../../languageHandler";
 
 const VectorAuthFooter = (): ReactElement => {
-    const brandingConfig = SdkConfig.getObject("branding");
-    const links = brandingConfig?.get("auth_footer_links") ?? [
-        { text: "Blog", url: "https://element.io/blog" },
-        { text: "Twitter", url: "https://twitter.com/element_hq" },
-        { text: "GitHub", url: "https://github.com/vector-im/element-web" },
-    ];
+    // const brandingConfig = SdkConfig.getObject("branding");
+    // const links = brandingConfig?.get("auth_footer_links") ?? [
+    //     { text: "Blog", url: "https://element.io/blog" },
+    //     { text: "Twitter", url: "https://twitter.com/element_hq" },
+    //     { text: "GitHub", url: "https://github.com/vector-im/element-web" },
+    // ];
 
-    const authFooterLinks: JSX.Element[] = [];
-    for (const linkEntry of links) {
-        authFooterLinks.push(
-            <a href={linkEntry.url} key={linkEntry.text} target="_blank" rel="noreferrer noopener">
-                {linkEntry.text}
-            </a>,
-        );
-    }
+    // const authFooterLinks: JSX.Element[] = [];
+    // for (const linkEntry of links) {
+    //     authFooterLinks.push(
+    //         <a href={linkEntry.url} key={linkEntry.text} target="_blank" rel="noreferrer noopener">
+    //             {linkEntry.text}
+    //         </a>,
+    //     );
+    // }
 
     return (
         <footer className="mx_AuthFooter" role="contentinfo">
-            {authFooterLinks}
+            {/* {authFooterLinks} */}
             <a href="https://matrix.org" target="_blank" rel="noreferrer noopener">
                 {_t("powered_by_matrix")}
             </a>
